e8279921003861dce6784af168995396c1aa5af7
victor <victor.njutha@student.moringaschool.com>
2023-10-06 15:57:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+//Employee name and address 
+const employee = {
+    name: "Victor Mwangi",
+    streetAddress: "Koinange st",
+  };
+  
+  // update employee 
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+      ...employee,
+      [key]: value,
+    };
+}
+
+    // update employee destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+
+  //  delete property 
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // delete property destructively
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
